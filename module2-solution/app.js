567741c22d481afa49c9df7d08cc5ba5e279adc0
Boris Sokolov <boris.sokolov@gmail.com>
2017-01-18 01:12:31 +0200

    Adding test data

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,12 +1,36 @@
 (function() {
     'use strict';
 
+    var shoppingList = [
+        {
+            name: "Milk",
+            quantity: "2"
+        },
+        {
+            name: "Donuts",
+            quantity: "200"
+        },
+        {
+            name: "Cookies",
+            quantity: "300"
+        },
+        {
+            name: "Chocolate",
+            quantity: "5"
+        },
+        {
+            name: "Tea",
+            quantity: "10"
+        }
+    ];
+
+
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController ', ToBuyController)
         .controller('AlreadyBoughtController ', AlreadyBoughtController)
         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    LunchRecommendationsController.$inject = ['$scope'];
+    ToBuyController.$inject = ['$scope'];
 
     function LunchRecommendationsController($scope) {
 
