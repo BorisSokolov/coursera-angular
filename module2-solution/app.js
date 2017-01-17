980888daf2388bc75084634e400c265a712d4f45
Boris Sokolov <boris.sokolov@gmail.com>
2017-01-18 01:17:57 +0200

    Adding injections

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     var shoppingList = [
@@ -30,41 +30,8 @@
         .controller('AlreadyBoughtController ', AlreadyBoughtController)
         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    ToBuyController.$inject = ['$scope'];
-
-    function LunchRecommendationsController($scope) {
-
-        var enjoyMsg = "Enjoy!";
-        var tooMuchMsg = "Too much!";
-        var errorMsg = "Please enter data first";
-
-        $scope.displayRecommendation = function() {
-            var recommendationString = getRecommendations($scope.list);
-            $scope.recommendation = recommendationString;
-        };
-
-
-        function getRecommendations(string) {
-            if (!string)
-                return errorMsg;
-
-            var listToEat = string.split(',');
-            var numberOfItems = 0;
-
-            for (var i in listToEat) {
-                if (listToEat[i].trim())
-                    numberOfItems++;
-            }
-
-            if (numberOfItems == 0)
-                return errorMsg;
-            if (numberOfItems <= 3)
-                return enjoyMsg;
-
-            return tooMuchMsg;
-        }
-
-    }
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 
 
 })();
