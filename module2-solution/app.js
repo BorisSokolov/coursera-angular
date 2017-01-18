54eb4878b23f041eab5358c24ba82c454c3a9b28
Boris Sokolov <boris.sokolov@gmail.com>
2017-01-19 00:37:57 +0200

    Solution

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,29 +1,5 @@
-(function () {
+(function() {
     'use strict';
-
-    var shoppingList = [
-        {
-            name: "Milk",
-            quantity: "2"
-        },
-        {
-            name: "Donuts",
-            quantity: "200"
-        },
-        {
-            name: "Cookies",
-            quantity: "300"
-        },
-        {
-            name: "Chocolate",
-            quantity: "5"
-        },
-        {
-            name: "Tea",
-            quantity: "10"
-        }
-    ];
-
 
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController ', ToBuyController)
@@ -33,5 +9,60 @@
     ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
     AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 
+    function ShoppingListCheckOffService() {
+        var service = this;
 
+        var toBuyItems = [{
+                name: "Milk",
+                quantity: "2"
+            },
+            {
+                name: "Donuts",
+                quantity: "200"
+            },
+            {
+                name: "Cookies",
+                quantity: "300"
+            },
+            {
+                name: "Chocolate",
+                quantity: "5"
+            },
+            {
+                name: "Tea",
+                quantity: "10"
+            }
+        ];
+        var boughtItems = [];
+
+        service.GetToBuyItems = function() {
+            return toBuyItems;
+        };
+
+        service.GetBoughtItems = function() {
+            return boughtItems;
+        };
+
+        service.buyItem = function(itemIndex) {
+            if (itemIndex > -1) {
+                var item = toBuyItems[itemIndex];
+                boughtItems.push(item);
+                toBuyItems.splice(itemIndex, 1);
+            }
+        };
+
+
+        function ToBuyController(ShoppingListCheckOffService) {
+            var buyList = this;
+            buyList.items = ShoppingListCheckOffService.GetItemsToBuy();
+            buyList.buyItem = function(index) {
+                ShoppingListCheckOffService.buyItem(index);
+            };
+        };
+
+        function AlreadyBoughtController(ShoppingListCheckOffService) {
+            var boughtList = this;
+            boughtList.items = ShoppingListCheckOffService.GetBoughtItems();
+        };
+    };
 })();
